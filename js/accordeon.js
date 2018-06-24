074b7c2d62d824dd77a6f2c7fbf5860d28924a26
chOkey <chokey1992@gmail.com>
2018-06-24 22:17:57 +0300

    commit 3
    Add new blocks

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,27 +1,8 @@
-var service_menu = document.getElementByclass('service_menu');
-		service_menu.addEventListener('click', change);
-		function change(event) {
-			var targ = event.target;
-			if (targ.tagName !== 'li') return; 
-			if (targ.classList.contains('select')) {
-				hideAll();
-			} else {
-				hideAll();
-				targ.classList.add('select');
-				showText(targ.nextElementSibling);
-			}
-		}
-		function hideAll() {
-			var liEl = service_menu.querySelectorAll('li');
-			var UlEl = service_menu.querySelectorAll('ul');
-			for (var i = 0; i < h3El.length; i++) {
-				h3El[i].classList.remove('select');
-			}
-			for (var i = 0; i < divEl.length; i++) {
-				divEl[i].style.height = '0';
-			}
-		}
-		function showText(textEl) {
-			textEl.style.height = textEl.scrollHeight + 'px';
-		}
-	
+$(document).ready(function() {
+  $('#accordeon .acc-head').on('click', f_acc);
+});
+ 
+function f_acc(){
+  $('#accordeon .acc-body').not($(this).next()).slideUp(1);
+    $(this).next().slideToggle(1);
+}
